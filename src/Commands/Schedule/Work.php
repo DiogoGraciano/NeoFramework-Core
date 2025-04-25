389c64c26af1bc0068a9355a44afc60f760e80d7
Diogo Graciano <diogo.dg691@gmail.com>
2025-04-25 11:11:27 -0300

    Adicionado quebra de linha

--- src/Commands/Schedule/Work.php
+++ src/Commands/Schedule/Work.php
@@ -22,13 +22,13 @@ class Work extends Command
 
         try{
             if(!file_exists(Functions::getRoot()."schedule.php")){
-                echo $color->error("Config file not found in ".Functions::getRoot()."schedule.php");
+                echo $color->error("Config file not found in ".Functions::getRoot()."schedule.php").PHP_EOL;
                 return;
             }
 
             require_once Functions::getRoot()."schedule.php";
 
-            echo $color->ok(json_encode(Scheduler::run(),JSON_PRETTY_PRINT));
+            echo $color->ok(json_encode(Scheduler::run(),JSON_PRETTY_PRINT)).PHP_EOL;
         }
         catch(Exception $e){
             echo $color->error($e->getMessage().PHP_EOL.$e->getTraceAsString());
